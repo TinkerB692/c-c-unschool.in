4ea8f8e641b40c566faa91425db6c278fa7d811f
TinkerB692 <84813618+TinkerB692@users.noreply.github.com>
2021-06-01 00:02:59 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//High or Low game
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	int num, guess, tries=5;
+	srand(time(0));
+	num = rand() % 50 + 1;
+	cout << "\n\nGuess My Number Game. You have 5 chances!\n\n";
+
+	do
+	{
+		cout << "\nEnter a guess between 1 and 50 : ";
+		cin >> guess;
+		tries--;
+		cout<<"\nNo. of chances left:"<<tries;
+
+		if (guess > num)
+			cout << "\nToo high!Enter a number lower than "<<guess<<"\n";
+		else if (guess < num)
+			cout << "\n\nToo low!Enter a number higher than "<<guess<<"\n";
+		else
+			cout << "\nCorrect! You Won!!\n";
+	}
+	while (tries != 0 && guess!=num);
+	cout<<"\nGame over!!\n";
+
+
+	return 0;
+}
